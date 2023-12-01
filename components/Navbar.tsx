b375a03ac84001ab3298b97ad27e8cecf0c50e70
sahil <sahilmulani501@gmail.com>
2023-12-02 00:58:48 +0530

    changed name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-[60px] m-0 p-2 bg-black fixed top-0 right-0">
         <nav className="flex justify-between items-center">
-            <p className="text-xl text-white font-sans font-semibold ml-5 p-1">xauth</p>
+            <p className="text-xl text-white font-sans font-semibold ml-5 p-1"><span className="text-red-500">Red</span>Shield</p>
             <ul className="text-slate-300 flex gap-4 mr-6 justify-center items-center">
                 <li>
                     <Link href={'/'}>About</Link>
