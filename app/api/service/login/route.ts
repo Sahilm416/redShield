759782688eeb93a8b2b127ecfd217c7abaeec112
sahil <sahilmulani501@gmail.com>
2023-12-02 11:56:44 +0530

    try

--- app/api/service/login/route.ts
+++ app/api/service/login/route.ts
@@ -1,19 +1,13 @@
-import { NextRequest,NextResponse } from "next/server"
-import { db } from "@/utils/database/db"
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/utils/database/db";
 
+export const POST = async (request: Request) => {
+  const key = request.headers.get("authorization") as string;
+  const res = await db.get(key);
+  if (!res) {
+    return NextResponse.json({ message: "Unauthorized key" }, { status: 401 });
+  } else {
+  }
 
-
-export const POST = async (request : Request)=>{
-       const key = request.headers.get("authorization") as string;
-       const res = await db.get(key);
-       if(!res){
-              return NextResponse.json({ message: "Unauthorized key"} , {status: 401})
-       }
-      else {
-             
-      }
-     
-       return NextResponse.json({message: "Success"} , {status:200})
-       
-
-}
+  return NextResponse.json({ message: "Success" }, { status: 200 });
+};
