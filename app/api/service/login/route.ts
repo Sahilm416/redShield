b998ab91d7593d2bb0b303e3ab58fddda54d0b1c
sahil <sahilmulani501@gmail.com>
2023-12-12 08:03:47 +0530

    Login with encrypted password hash

--- app/api/service/login/route.ts
+++ app/api/service/login/route.ts
@@ -1,15 +1,34 @@
-const { NextRequest, NextResponse } = require("next/server");
+const { NextResponse } = require("next/server");
 const { db } = require("@/utils/database/db");
+const bcrypt = require("bcrypt");
 
 export const POST = async (request: Request) => {
+
+  const data = await request.json();
   const key = request.headers.get("authorization") as string;
   const res = await db.get(key);
+
   if (!res) {
     return NextResponse.json({ message: "Unauthorized key" }, { status: 401 });
   } else {
+    try {
+      const user = await db.get(data.username);
+      if (user) {
+        const isAuth = await bcrypt.compare(data.password, user.password);
+        if (isAuth) {
+          return NextResponse.json(
+            { message: "Login Success" },
+            { status: 200 }
+          );
+        }
+      }
+    } catch (error) {
+      return NextResponse.json(
+        { message: "something went wrong" },
+        { status: 404 }
+      );
+    }
   }
 
-  return NextResponse.json({ message: "Success" }, { status: 200 });
+  return NextResponse.json({ message: "Invalid Credentials" }, { status: 401 });
 };
-
-
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,31 +4,11 @@ import { Button } from "./ui/button"
 
 const Hero = () => {
   const token = "rgcr464647xrgrgcvrc"
-  const check = async ()=>{
-    try {
-      console.log("clicked")
-      const res =  await fetch("/api/service/login",{
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "authorization": `${token}`
-        },
-        body: JSON.stringify({
-          username: "sahil_007",
-          password: "12345"
-        })
-      })
-      console.log(res)
-      
-    } catch (error) {
-      console.log(error)
-    }
-}
   
   return (
     <div className="w-full h-[100vh] flex flex-col justify-center items-start p-5">
         <p className="text-5xl font-sans font-bold text-blue-500 p-3 "><span className="text-slate-800">Redis</span> based <br /> authentication library </p>
-        <Button onClick={check} className="mx-4 w-[200px] h-[45px] rounded-none my-5">Explore now</Button>  
+        <Button className="mx-4 w-[200px] h-[45px] rounded-none my-5">Explore now</Button>  
         <p  className="text-3xl mx-4 p-1">comming soon...</p>
     </div>
   )
