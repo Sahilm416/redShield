ea7984195454a863d9a59490519fac20d33e834d
sahil <sahilmulani501@gmail.com>
2023-12-16 23:00:20 +0530

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='bg-slate-200'>
+      <body className=''>
         <Navbar/>
         {children}
         <Toaster richColors position="bottom-right" />
