4ca9ee60c13866e6301e70f81879f192195f59d7
sahil <sahilmulani501@gmail.com>
2023-12-16 22:37:38 +0530

    minor fix

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -2,7 +2,7 @@ import { AuthPage } from "@/components/Auth"
 
 export default function LoginPage (){
     return (
-        <div className="w-full h-screen flex flex-col items-center mt-[100px]">
+        <div className="w-full h-screen flex flex-col items-center pt-[100px]">
             <AuthPage/>
             
         </div>
