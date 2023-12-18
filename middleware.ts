5674eb7dbbb59d927451771f56762c35258c04e6
sahil <sahilmulani501@gmail.com>
2023-12-18 14:00:42 +0530

    server error fix try 1

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
-//for some reason importing this causes error so instead of import we use require
-const { sign, verify } = require("jsonwebtoken");
-export const dynamic = 'force-dynamic'
+
 export default async function middleWare(request: NextRequest) {
   const cookie = getCookie("_auth_token", { cookies });
 
-  const url = request.nextUrl.clone();
-  url.pathname = "/api/service/verify";
-  const res = await fetch(url, {
+ 
+  const res = await fetch("https://redshield.vercel.app/api/service/verify", {
     cache: 'no-store' ,
     next: {revalidate: 0},
     method: "POST",
