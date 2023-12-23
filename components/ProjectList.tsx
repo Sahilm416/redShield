529c077164cbce7c7c8cec66fc307e74101e088d
sahil <sahilmulani501@gmail.com>
2023-12-24 00:53:18 +0530

    fix/bg color fix for project list

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -4,37 +4,37 @@ import { Card ,CardHeader, CardTitle ,CardContent ,CardDescription, CardFooter }
 export default function ProjectList() {
   return (
     <main className="w-full max-w-[750px] grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2 sm:px-2 px-5">
-       <Card className="bg-slate-300 m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300 m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
         </CardHeader>
        </Card>
-       <Card className="bg-slate-300 m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300 m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
         </CardHeader>
        </Card>
-       <Card className="bg-slate-300  m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300  m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
         </CardHeader>
        </Card>
-       <Card className="bg-slate-300 m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300 m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
         </CardHeader>
        </Card>
-       <Card className="bg-slate-300 m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300 m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
         </CardHeader>
        </Card>
-       <Card className="bg-slate-300 m-3 dark:bg-slate-900 dark:text-slate-300 text-slate-800">
+       <Card className="bg-slate-300 m-3 dark:bg-black dark:text-slate-300 text-slate-800">
         <CardHeader>
             <CardTitle>Project 1</CardTitle>
             <CardDescription >info about projet</CardDescription>
