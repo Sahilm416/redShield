224752e5feeba880a76850fc772679721aee184c
sahil <sahilmulani501@gmail.com>
2023-12-23 20:55:09 +0530

    test/fixing server action try 2

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -4,10 +4,10 @@ import { getUser } from "../actions/auth";
 import { LoggedUser } from "../actions/auth";
 export default async function DashboardPage() {
   const res = await LoggedUser();
-  const user = await getUser({username: res.data}) as {username: string , email: string , isVerified: boolean};
+ 
   return (
     <div className="w-full flex flex-col justify-start items-center mt-[100px]">
-        <DashboardComponent username={user.username} isVerified={user.isVerified} />
+        <DashboardComponent username={res.data} isVerified={res.status} />
         
         <ProjectList/>
     </div>
