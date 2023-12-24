b15175841b30cc7d6bacc8245ea55b87f1811bd6
sahil <sahilmulani501@gmail.com>
2023-12-24 16:19:38 +0530

    fix/error fix try 2

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,55 +1,10 @@
-"use client";
-import Profile from "@/components/Profile";
-import { LoggedUser, getUserInfo } from "../actions/auth";
-import { useEffect, useState } from "react";
-import ProjectList from "@/components/ProjectList";
-import NotVerified from "@/components/NotVerifield";
-
-type Project = {
-  name: string;
-  description: string;
-  created_at: string;
-  key: string;
-};
-
-type userData = {
-  username: string;
-  email: string;
-  isVerified: boolean;
-  profile_picture?: string;
-  projects: Project[];
-};
-
-export default function DashboardPage() {
-  const [data, setData] = useState<userData>();
-
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  const loadData = async () => {
-   const res = await LoggedUser();
-   console.log("res is " + res);
-    const user = await getUserInfo({ username: res.data });
-    console.log("user is " + user);
-    setData(user);
-  };
-
-  return (
-    <div className="w-full flex flex-col gap-5 justify-start items-center mt-[100px]">
-      {!data ? (
-        "Loading..."
-      ) : (
-        <>
-          <Profile
-            username={data?.username}
-            email={data?.email}
-            isVerified={data?.isVerified}
-            profile_picture={data?.profile_picture}
-          />
-          {data.isVerified ? (<ProjectList projects={data.projects}  />) : (<NotVerified />)}
-        </>
-      )}
-    </div>
-  );
+import { LoggedUser } from "../actions/auth";
+import DashboardComponent from "@/components/Dashboard";
+export default async function (){
+  const res = await LoggedUser();
+  return(
+    <>
+      <DashboardComponent username={res.data}/>
+    </>
+  )
 }
