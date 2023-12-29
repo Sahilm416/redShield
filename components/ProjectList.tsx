99c9a02c160eb03ffbf4e1c613df834ec50dea67
sahil <sahilmulani501@gmail.com>
2023-12-29 18:46:02 +0530

    fix/css and projects

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
-import { Card, CardHeader, CardTitle, CardDescription,CardContent, CardFooter} from "./ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 type Project = {
-  id: string
+  id: string;
   name: string;
   description: string;
   created_at: string;
@@ -13,42 +20,50 @@ type Project = {
 export default function ProjectList({ projects }: { projects: Project[] }) {
   return (
     <>
-      {(projects.length > 0) ? (
+      {projects.length > 0 ? (
         <main className="flex flex-col w-full min-h-screen p-4 md:gap-8 md:p-10">
-        <div className="max-w-6xl w-full mx-auto flex items-center gap-4">
-          <form className="flex-1">
-            <Input className="bg-white dark:bg-black" placeholder="Search projects..." />
-            <Button className="sr-only" type="submit">
-              Submit
-            </Button>
-          </form>
-          <Link href={'/New'}><Button>Add New Project</Button></Link>
-        </div>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl w-full mx-auto">
-          {projects.map((project,i)=>{
-            return (
-              <Card key={i} className=" bg-white dark:bg-black">
-            <CardHeader className="flex flex-row items-center gap-4">
-       
-              <div className="grid gap-1">
-                <CardTitle>{project.name}</CardTitle>
-                <CardDescription>{project.description}</CardDescription>
-              </div>
-            </CardHeader>
-            <CardFooter>
-            <Link className="w-full" href={`/Project/${project.id}`}><Button variant={'outline'} className="w-full bg-gray-400/20 dark:bg-gray-700/80 dark:hover:bg-slate-700" >
-              view
-            </Button></Link>
-            </CardFooter>
-          </Card>
-            )
-          })}
-        </div>
-      </main>
+          <div className="max-w-6xl w-full mx-auto flex items-center gap-4">
+            <form className="flex-1">
+              <Input
+                className="bg-white dark:bg-black"
+                placeholder="Search projects..."
+              />
+              <Button className="sr-only" type="submit">
+                Submit
+              </Button>
+            </form>
+            <Link href={"/New"}>
+              <Button>Add New Project</Button>
+            </Link>
+          </div>
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl w-full mx-auto">
+            {projects.map((project, i) => {
+              return (
+                <Card key={i} className=" bg-white dark:bg-black">
+                  <CardHeader className="flex flex-row items-center gap-4">
+                    <div className="grid gap-1">
+                      <CardTitle>{project.name}</CardTitle>
+                      <CardDescription>{project.description}</CardDescription>
+                    </div>
+                  </CardHeader>
+                  <CardFooter>
+                    <Link className="w-full" href={`/Project/${project.id}`}>
+                      <Button
+                        variant={"outline"}
+                        className="w-full bg-gray-400/20 dark:bg-gray-700/80 dark:hover:bg-slate-700"
+                      >
+                        view
+                      </Button>
+                    </Link>
+                  </CardFooter>
+                </Card>
+              );
+            })}
+          </div>
+        </main>
       ) : (
         <p className="text-center text-slate-400 text-xl mt-5">No Projects</p>
       )}
     </>
   );
 }
-
