e96224c66ea196ab4833b8a39d42a56bf0c0e0a3
sahil <sahilmulani501@gmail.com>
2023-12-30 21:25:55 +0530

    add/sign in with google

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -2,7 +2,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LoginCard from "./LoginCard";
 import RegisterCard from "./RegisterCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { useSession } from "next-auth/react";
 import { checkGoogleUserExists } from "@/app/actions/auth";
@@ -14,32 +14,40 @@ export function AuthPage() {
       description: "provide the credentials to login",
     });
   }, []);
+
   const router = useRouter();
   const user = useSession();
+  const signGoogleUser = async () => {
+    if (user.status === "authenticated") {
+      const email = user.data.user?.email as string;
+      const profile = user.data.user?.image as string;
 
-
-  if (user.status === "authenticated") {
-    const email = user.data.user?.email as string;
-    const profile = user.data.user?.image as string;
-    const signGoogleUser = async () => {
       await checkGoogleUserExists({ email: email, profile_picture: profile });
       return router.push("/Dashboard");
-    };
-    signGoogleUser();
-  }
+    }
+  };
 
-  return (
-    <Tabs className=" w-[90vw] max-w-[400px]" defaultValue="login">
-      <TabsList className="grid dark:bg-slate-900 w-full grid-cols-2">
-        <TabsTrigger value="login">Login</TabsTrigger>
-        <TabsTrigger value="register">Register</TabsTrigger>
-      </TabsList>
-      <TabsContent value="login">
-        <LoginCard />
-      </TabsContent>
-      <TabsContent value="register">
-        <RegisterCard />
-      </TabsContent>
-    </Tabs>
-  );
+  if (user.status === "authenticated") {
+    signGoogleUser();
+    return (
+      <div className="w-full mt-[100px] grid place-items-center">
+        <p>Redirecting user...</p>
+      </div>
+    );
+  } else {
+    return (
+      <Tabs className=" w-[90vw] max-w-[400px]" defaultValue="login">
+        <TabsList className="grid dark:bg-slate-900 w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginCard />
+        </TabsContent>
+        <TabsContent value="register">
+          <RegisterCard />
+        </TabsContent>
+      </Tabs>
+    );
+  }
 }
