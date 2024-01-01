21237a052212f14d3a65c91b1f1971c229d62c55
sahil <sahilmulani501@gmail.com>
2024-01-01 15:26:27 +0530

    add/image in project list

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -10,6 +10,8 @@ import {
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { imageLinks } from "@/public/images";
+
 type Project = {
   id: string;
   name: string;
@@ -50,7 +52,7 @@ export default function ProjectList({ projects }: { projects: Project[] }) {
                   <CardContent className="py-2">
                     <div className="w-full h-[150px] overflow-hidden select-none">
                       <Image
-                        src="https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={imageLinks[i]}
                         width={400}
                         height={200}
                         alt="Picture of the author"
--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
             hostname: 'images.unsplash.com',
           
           },
+          {
+            protocol: 'https',
+            hostname: 'plus.unsplash.com',
+          
+          },
         ],
       },
 }
