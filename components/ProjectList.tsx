94d28b824f618effd72d45d64cecaaeb5e940dae
sahil <sahilmulani501@gmail.com>
2024-01-02 00:55:44 +0530

    add/delete option to project list

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -14,6 +14,16 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { MoreVertical } from "lucide-react";
 import { Input } from "./ui/input";
@@ -60,59 +70,88 @@ export default function ProjectList({ projects }: { projects: Project[] }) {
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 sm:max-w-6xl max-w-[450px] w-full mx-auto">
             {filteredProjects.map((project, i) => {
               return (
-                <Card
-                  key={i}
-                  className=" bg-white dark:bg-gray-900/20 shadow-md rounded-none"
-                >
-                  <CardHeader className="gap-4 pb-2  ">
-                    <div className="grid gap-2">
-                      <CardTitle className="flex justify-between items-center">
-                        <p>{project.name}</p>
-                        <DropdownMenu>
-                          <DropdownMenuTrigger className=" focus:outline-none">
-                            <span className=" cursor-pointer">
-                              <MoreVertical />
-                            </span>
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent>
-                            <DropdownMenuItem className="text-md">
-                              Delete
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="text-md">
-                              Edit
-                            </DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                      </CardTitle>
-                      <CardDescription>{project.description}</CardDescription>
-                    </div>
-                  </CardHeader>
-                  <CardContent className="py-2">
-                    <div className="w-full h-[150px] overflow-hidden select-none">
-                      <Avatar>
-                        <AvatarImage
-                          src={imageLinks[i]}
-                          width={400}
-                          height={200}
-                          alt="Picture of the author"
-                        />
-                        <AvatarFallback className=" w-full h-full flex justify-center items-center text-slate-300">
-                          Loading...
-                        </AvatarFallback>
-                      </Avatar>
-                    </div>
-                  </CardContent>
-                  <CardFooter>
-                    <Link className="w-full" href={`/Project/${project.id}`}>
-                      <Button
-                        variant={"outline"}
-                        className="w-full rounded-none"
-                      >
-                        view
+                <Drawer>
+                  <Card
+                    key={i}
+                    className=" bg-white dark:bg-gray-900/20 shadow-md rounded-none"
+                  >
+                    <CardHeader className="gap-4 pb-2  ">
+                      <div className="grid gap-2">
+                        <CardTitle className="flex justify-between items-center">
+                          <p>{project.name}</p>
+                          <DropdownMenu>
+                            <DropdownMenuTrigger className=" focus:outline-none">
+                              <span className=" cursor-pointer">
+                                <MoreVertical />
+                              </span>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                              <DropdownMenuItem className="text-md">
+                                <DrawerTrigger className="w-full h-full text-start">
+                                  Delete
+                                </DrawerTrigger>
+                              </DropdownMenuItem>
+
+                              <DropdownMenuItem className="text-md">
+                                Edit
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </CardTitle>
+                        <CardDescription>{project.description}</CardDescription>
+                      </div>
+                    </CardHeader>
+                    <CardContent className="py-2">
+                      <div className="w-full h-[150px] overflow-hidden select-none">
+                        <Avatar>
+                          <AvatarImage
+                            src={imageLinks[i]}
+                            width={400}
+                            height={200}
+                            alt="Picture of the author"
+                          />
+                          <AvatarFallback className=" w-full h-full flex justify-center items-center text-slate-300">
+                            Loading...
+                          </AvatarFallback>
+                        </Avatar>
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                      <Link className="w-full" href={`/Project/${project.id}`}>
+                        <Button
+                          variant={"outline"}
+                          className="w-full rounded-none"
+                        >
+                          view
+                        </Button>
+                      </Link>
+                    </CardFooter>
+                  </Card>
+                  <DrawerContent className="flex justify-center items-center flex-col h-[40vh] bg-white dark:bg-black">
+                    <DrawerHeader>
+                      <DrawerTitle className=" text-center">
+                        Are tou sure ?
+                      </DrawerTitle>
+                      <DrawerDescription className="text-center">
+                        This action can't be undone.
+                      </DrawerDescription>
+                    </DrawerHeader>
+                    <p className=" dark:text-slate-200 text-slate-500">
+                      Delete project{" "}
+                      <span className="text-slate-600 dark:text-slate-100 font-bold">
+                        {project.name}
+                      </span>
+                    </p>
+                    <div className="flex sm:flex-row gap-8 my-10">
+                      <Button className=" rounded-none" variant={"destructive"}>
+                        Delete
                       </Button>
-                    </Link>
-                  </CardFooter>
-                </Card>
+                      <DrawerClose>
+                        <Button className=" rounded-none">Cancel</Button>
+                      </DrawerClose>
+                    </div>
+                  </DrawerContent>
+                </Drawer>
               );
             })}
           </div>
