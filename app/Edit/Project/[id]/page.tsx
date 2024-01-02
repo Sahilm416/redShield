d3a965df1d1d4498f269e2b209d1269b3bf42c1c
sahil <sahilmulani501@gmail.com>
2024-01-03 01:13:24 +0530

    fix/css

--- app/Edit/Project/[id]/page.tsx
+++ app/Edit/Project/[id]/page.tsx
@@ -11,8 +11,8 @@ type projectData = {
 export default async function ({ params }: { params: { id: string } }) {
   const project = (await getProject({ id: params.id })) as projectData;
   return (
-    <div className="w-full h-screen flex justify-center mt-[100px] sm:mt-0 sm:items-center">
-      {project && <UpdateProject project={project} />}
-    </div> 
+    <div className="w-full sm:h-screen flex justify-center mt-[100px] sm:mt-0 sm:items-center">
+      <UpdateProject project={project} />
+    </div>
   );
 }
