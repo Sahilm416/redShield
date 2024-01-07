a47968f9b0404cab1c67343e73a969b218483b90
sahil <sahilmulani501@gmail.com>
2024-01-07 09:52:05 +0530

    fixed auth page render on login

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -1,9 +1,11 @@
-import { AuthPage } from "@/components/Auth"
-export const revalidate = 0;
-export default function LoginPage (){
-    return (
-        <div className="w-full h-screen flex flex-col items-center pt-[100px] overflow-y-hidden">
-            <AuthPage/>
-        </div>
-    )
+import { AuthPage } from "@/components/Auth";
+import { getSession } from "../actions/auth";
+
+export default async function LoginPage() {
+  const session = await getSession();
+  return (
+    <div className="w-full h-screen flex flex-col items-center pt-[100px] overflow-y-hidden">
+      <AuthPage loginStatus={session.status} />
+    </div>
+  );
 }
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -2,30 +2,40 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LoginCard from "./LoginCard";
 import RegisterCard from "./RegisterCard";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { getSession } from "@/app/actions/auth";
 
+export function AuthPage({ loginStatus }: { loginStatus: boolean }) {
+  const router = useRouter();
+  if (loginStatus) {
+    router.push("/Dashboard");
+    return (
+      <div className="flex w-full h-screen justify-center items-center">
+        <p className="text-2xl">Redirecting...</p>
+      </div>
+    );
+  }
 
-export function AuthPage() {
   useEffect(() => {
     toast.message("Login with your redshield account", {
       description: "provide the credentials to login",
     });
   }, []);
 
-    return (
-      <Tabs className=" w-[90vw] max-w-[400px]" defaultValue="login">
-        <TabsList className="grid dark:bg-slate-900 w-full grid-cols-2">
-          <TabsTrigger value="login">Login</TabsTrigger>
-          <TabsTrigger value="register">Register</TabsTrigger>
-        </TabsList>
-        <TabsContent value="login">
-          <LoginCard />
-        </TabsContent>
-        <TabsContent value="register">
-          <RegisterCard />
-        </TabsContent>
-      </Tabs>
-    );
-  
+  return (
+    <Tabs className=" w-[90vw] max-w-[400px]" defaultValue="login">
+      <TabsList className="grid dark:bg-slate-900 w-full grid-cols-2">
+        <TabsTrigger value="login">Login</TabsTrigger>
+        <TabsTrigger value="register">Register</TabsTrigger>
+      </TabsList>
+      <TabsContent value="login">
+        <LoginCard />
+      </TabsContent>
+      <TabsContent value="register">
+        <RegisterCard />
+      </TabsContent>
+    </Tabs>
+  );
 }
