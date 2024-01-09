85ec811cd5beca1d1b17ab736a6a7f19daf71334
sahil <sahilmulani501@gmail.com>
2024-01-09 17:12:47 +0530

    refactor : verify email template

--- components/emailTemplates/verifyMail.ts
+++ components/emailTemplates/verifyMail.ts
@@ -13,59 +13,56 @@ export const verifyMail = async ({
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verification Code</title>
+    <title>Verification Code from Redshield</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
+            background-color: #f8f8f8;
         }
 
         .container {
-            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
             padding: 20px;
-            box-sizing: border-box;
+            background-color: #ffffff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
-        .title {
+        .heading {
             font-size: 24px;
             font-weight: bold;
-            margin-bottom: 10px;
+            margin-bottom: 20px;
         }
 
-        .content {
-            font-size: 16px;
-            margin-bottom: 10px;
+        .subheading {
+            font-size: 18px;
+            margin-bottom: 20px;
         }
 
-        .verification-code {
-            font-size: 20px;
+        .code {
+            font-size: 24px;
             font-weight: bold;
-            color: #4A90E2;
-            margin-bottom: 10px;
+            margin-bottom: 20px;
         }
 
-        .note {
+        .footer {
             font-size: 14px;
             color: #888;
+            margin-top: 20px;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <div class="title">${project} Verification Code</div>
-        <div class="content">
-            Enter the following verification code when prompted:
-        </div>
-        <div class="verification-code">${code}</div>
-        <div class="content">
-            To protect your account, do not share this code.
-        </div>
-        <div class="content">
-            Didn't request this? This code was requested from 45.118.104.235, Mumbai, IN at 12 August 2023, 12:46 UTC. If you didn't make this request, you can safely ignore this email.
-        </div>
+        <div class="heading">${project} Verification Code</div>
+        <div class="subheading">Enter the following verification code when prompted:</div>
+        <div class="code">${code}</div>
+        <div class="subheading">To protect your account, do not share this code.</div>
+        <div class="footer">Didn't request this? This code was requested from 45.118.104.235, Mumbai, IN at 12 August 2023, 12:46 UTC. If you didn't make this request, you can safely ignore this email.</div>
     </div>
 </body>
-</html>  
+</html>
     `;
 };
