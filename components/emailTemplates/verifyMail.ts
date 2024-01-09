a8680e9ef181866b595724ef08c530ce47fb5435
sahil <sahilmulani501@gmail.com>
2024-01-09 17:04:03 +0530

    refactor : verify email template

--- components/emailTemplates/verifyMail.ts
+++ components/emailTemplates/verifyMail.ts
@@ -9,30 +9,63 @@ export const verifyMail = async ({
 }) => {
   return `
   <!DOCTYPE html>
-  <html lang="en">
-  <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email Verification</title>
-  </head>
-  <body style="font-family: 'Arial', sans-serif; background-color: #fff;">
-  
-    <div style=" margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 5px; background-color: white;">
-      <h2 style="color: #333;">Email Verification for ${project}</h2>
-      <p>Hello <b>${email}</b>,</p>
-      <p>Thank you for signing up with <b> ${project}</b> . To complete your registration, please use the following verification code:</p>
-      <p style="font-size: 24px; color: #4caf50; padding: 10px; background-color: #f5f5f5; border-radius: 5px; text-align: center; margin: 20px 0;">${code}</p>
-      <p>Please enter this code in the verification page</p>
-  
-      <p>This code will expire in 3 minutes.</p>
-      <p>If you did not sign up for <b> ${project}</b> , please ignore this email.</p>
-      <p>Thank you,</p>
-      <p>${project}</p>
+    <title>Verification Code</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 100%;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+
+        .title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .content {
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+
+        .verification-code {
+            font-size: 20px;
+            font-weight: bold;
+            color: #4A90E2;
+            margin-bottom: 10px;
+        }
+
+        .note {
+            font-size: 14px;
+            color: #888;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="title">${project} Verification Code</div>
+        <div class="content">
+            Enter the following verification code when prompted:
+        </div>
+        <div class="verification-code">${code}</div>
+        <div class="content">
+            To protect your account, do not share this code.
+        </div>
+        <div class="content">
+            Didn't request this? This code was requested from 45.118.104.235, Mumbai, IN at 12 August 2023, 12:46 UTC. If you didn't make this request, you can safely ignore this email.
+        </div>
     </div>
-  
-  </body>
-  </html>
-  
-    
+</body>
+</html>  
     `;
 };
