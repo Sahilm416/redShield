3f895e293d18395ab52cb4032196f1fd76969205
sahil <sahilmulani501@gmail.com>
2024-01-09 21:39:09 +0530

    refactor : verify email template

--- components/emailTemplates/verifyMail.ts
+++ components/emailTemplates/verifyMail.ts
@@ -8,68 +8,57 @@ export const verifyMail = async ({
   project: string;
 }) => {
   return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Auth Verification Code</title>
-      <style>
-          body {
-              font-family: Arial, sans-serif;
-              margin: 0;
-              padding: 0;
-              background-color: #f8f8f8;
-          }
-  
-          .container {
-              max-width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-              background-color: #ffffff;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-          }
-  
-          .heading {
-              font-size: 24px;
-              font-weight: bold;
-              margin-bottom: 20px;
-              color: #000;
-          }
-  
-          .subheading {
-              font-size: 18px;
-              margin-bottom: 20px;
-              color: #666;
-          }
-  
-          .code {
-              font-size: 24px;
-              font-weight: bold;
-              margin-bottom: 20px;
-              padding: 10px 20px;
-              background-color: #f2f2f2;
-              color: #333;
-              border-radius: 5px;
-              text-align: center;
-          }
-  
-          .footer {
-              font-size: 14px;
-              color: #888;
-              margin-top: 20px;
-          }
-      </style>
-  </head>
-  <body>
-      <div class="container">
-          <div class="heading">${project} Verification Code</div>
-          <div class="subheading">Enter the following verification code when prompted:</div>
-          <div class="code">${code}</div>
-          <div class="subheading">To protect your account, do not share this code.</div>
-          <div class="footer">Didn't request this? This code was requested from 45.118.104.235, Mumbai, IN at 12 August 2023, 12:46 UTC. If you didn't make this request, you can safely ignore this email.</div>
-      </div>
-  </body>
-  </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Verify Email Code</title>
+  <style>
+    .content {
+      max-width: 400px;
+      height: 700px;
+      padding: 20px;
+      font-family: Sans-serif;
+      background-color: #f3f4f6;
+      display:flex;
+      flex-direction: column;
+      justify-content:center;
+      align-items:start;
+      
+    }
+    .project {
+      color: #475569;
+    }
+
+    .sub{
+      color:#475569;
+    }
+
+    .warning{
+color:#475569;
+    }
+    .footer {
+      color:#64748b;
+      font-size: 12px;
+      line-height: 20px;
+    }
+    .footer > b {
+        color: #1e293b;
+    }
+  </style>
+</head>
+<body>
+  <div class="content">
+    <h2 class="project">${project}</h2>
+    <h1 class="header">Verification Code</h1>
+    <p class="sub">Enter the following verification code when<br/> prompted:</p>
+    <h1 class="code">${code}</h1>
+    <p class="warning">To protect your account, do not share this code.</p>
+    <p class="footer"><b>Didn't request this?</b><br/>This code was requested from <b>1.1.1.1</b>, <b>Mumbai, IN</b>, at <b>12 August 2023</b>, <b>12:46 UTC</b>. If you didn't make this request, you can safely ignore this email.</p>
+  </div>
+</body>
+</html>
     `;
 };
