67e88d0fbe76203a3c2fdf77729e10aefeb6f814
sahil <sahilmulani501@gmail.com>
2024-01-20 23:33:34 +0530

    style : fix padding of search bar

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -55,9 +55,9 @@ export default function ProjectList({ projects }: { projects: Project[] }) {
     <>
       {projects.length > 0 ? (
         <main className="flex flex-col w-full p-4 gap-5 md:gap-8 md:p-10">
-          <div className="max-w-6xl w-full mx-auto flex items-center gap-4 sticky top-[60px] nav pt-3  p-2 z-[20]">
+          <div className="max-w-6xl w-full mx-auto flex items-center gap-4 sticky top-[60px] nav pt-3  py-2 z-[20]">
             <Input
-              className="bg-white dark:bg-black rounded-none border-[#EBEBEB] dark:border-[#1F1F1F]"
+              className="bg-white dark:bg-black rounded-none border-2 border-[#EBEBEB] dark:border-[#1F1F1F]"
               placeholder="Search projects..."
               value={searchTerm}  
               onChange={(e) => setSearchTerm(e.target.value)}
