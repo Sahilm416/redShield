be3cfcf137f19d651a26d35fc61d9164399bcc55
sahil <sahilmulani501@gmail.com>
2024-01-26 12:17:35 +0530

    refactor: register api for clients

--- app/api/service/register/route.ts
+++ app/api/service/register/route.ts
@@ -10,51 +10,47 @@ interface reqBody {
 }
 
 export const POST = async (request: Request) => {
+  const data: reqBody = await request.json();
+  const key = request.headers.get("authorization") as string;
+  const res = (await db.get("API_KEY:" + key)) as { project_id: string };
+
+  if (!res) {
+    return NextResponse.json({ message: "Unauthorized key" }, { status: 401 });
+  }
+
   try {
-    const data: reqBody = await request.json();
-    const key = request.headers.get("authorization") as string;
-    const res = await db.get("API_KEY:" + key);
-
-    if (!res) {
-
-      return NextResponse.json({ message: "Unauthorized key" }, { status: 401 });
-    }
-
-    const searchKey = res.project_id + ":" + data.email+":user";
-    const existingUser = await db.get(searchKey);
-   
-  
-    if (!existingUser) {
-      const hashedPassword = await bcrypt.hash(data.password, 10);
-      const newUser = {
-        uid: uuidv4(),
-        password: hashedPassword,
-        email: data.email,
-        profile_picture:
-          data.profile_picture ||
-          "https://vercel.com/api/www/avatar/e4HZrj63hu6L3DgyuIE06nf7?&s=64",
-        creation_date: new Date(),
-      };
-
-      // Set the new user data
-        await db.set(searchKey, newUser);
-
-        //Initialize empty project list
-     
-
-      return NextResponse.json({
-        status: true,
-        message: "Profile created successfully",
-      });
-    } else {
-      // Username already exists
+    const checkAlreadyExists = await db.get(
+      `${res.project_id}:${data.email}:user`
+    );
+    if (checkAlreadyExists) {
       return NextResponse.json({
         status: false,
-        message: "User profile already exists",
+        message: " user already exists",
       });
     }
+
+    const hashedPassword = await bcrypt.hash(data.password.trim(), 10);
+    const newUser = {
+      uid: uuidv4(),
+      password: hashedPassword,
+      email: data.email.trim(),
+      profile_picture:
+        data.profile_picture ||
+        "https://vercel.com/api/www/avatar/e4HZrj63hu6L3DgyuIE06nf7?&s=64",
+      creation_date: new Date(),
+      pwd_version: Date.now(),
+    };
+    await db.set(res.project_id + ":" + data.email + ":user", newUser);
+
+    return NextResponse.json({
+      status: true,
+      message: "Registered successfully",
+    });
   } catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+    console.log("error registering", error);
+    return NextResponse.json({
+      status: false,
+      message: "something went wrong",
+    });
   }
 };
