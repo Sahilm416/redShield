b3491440cf7228c8356d136867fd2345fd49a048
sahil <sahilmulani501@gmail.com>
2024-02-08 00:32:57 +0530

    style : minor css change

--- app/Docs/GetStarted/page.tsx
+++ app/Docs/GetStarted/page.tsx
@@ -107,7 +107,7 @@ export default function GettingStarted() {
               variant={"outline"}
               className="w-[100px] dark:border-[#171717] group/btn flex gap-2 rounded-none hover:bg-transparent"
             >
-              Next {" "} <ArrowRight className=" relative left-0 group-hover/btn:left-1"/>
+              Next {" "} <ArrowRight className=" relative left-0 group-hover/btn:left-1 transition-all fade-in-100"/>
             </Button>
           </Link>
         </div>
--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -6,11 +6,10 @@ export default async function Ammouncement() {
   return (
     <div className="h-[7vw] w-full flex justify-center items-center absolute mt-[20px] right-0 transition-all fade-in-100">
       <Link
-        className=""
         target="blanc"
         href={"https://www.npmjs.com/package/redshield"}
       >
-        <div className=" h-[40px] px-2 rounded-3xl bg-zinc-50 dark:bg-[#111111] border-zinc-200 dark:border-zinc-800 flex justify-center items-center gap-2 border">
+        <div className=" h-[40px] px-2 rounded-3xl bg-zinc-50 dark:bg-[#111111] border-zinc-200 dark:border-zinc-800 flex justify-center items-center gap-2 border transition-shadow fade-in-[400] hover:shadow-md">
           <span className="px-2 text-green-700 bg-green-100 border border-green-700 rounded-2xl">
             New
           </span>
