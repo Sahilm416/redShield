4c8f3d08bf39272629931e218f70301e57524dad
sahil <sahilmulani501@gmail.com>
2024-02-20 20:15:02 +0530

    test : redshield integration

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -1,11 +1,11 @@
-import { AuthPage } from "@/components/Auth";
-import { getSession } from "../actions/auth";
+import AuthPage from "redshield/Auth";
+import { getProject } from "redshield/actions/auth";
 
 export default async function LoginPage() {
-  const session = await getSession();
+  const project = await getProject();
   return (
     <div className="w-full h-[calc(100vh-60px)] flex flex-col items-center mt-[40px] overflow-y-hidden">
-      <AuthPage loginStatus={session.status} />
+      <AuthPage project={project} />
     </div>
   );
 }
