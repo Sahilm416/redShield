3c965ffcc21a0ea6f94bcf644050b54c540500d6
sahil <sahilmulani501@gmail.com>
2024-03-22 15:02:20 +0530

    fix : remove regex from middleware

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   //add routes in matcher array to protect them from unauthenticated users
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
     "/Dashboard",
     "/Auth",
     "/Project/:path*",
